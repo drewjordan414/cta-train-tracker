de7df8ce2688a3967367a3a15f62f0509aa20a2b
Drew Jordan <ajordan12@luc.edu>
2023-05-30 14:24:58 -0500

    thinkin

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,14 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
+// const mymap = L.map('mapid').setView([41.8781, -87.6298], 13);
+
+// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/{z}/{x}/{y}?access_token={accessToken}', {
+//   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//   maxZoom: 18,
+//   id: 'your-style-id',
+//   accessToken: mapBox,
+// }).addTo(mymap);
 
 // CTA API
 fetch('https://api.transitchicago.com/api/1.0/cta/' + API_ENDPOINT)
