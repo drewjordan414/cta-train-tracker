eb0219c69b00cd3918e31ced663361629a93c7ff
AlanDBalan <132862280+AlanDBalan@users.noreply.github.com>
2023-05-30 13:50:13 -0500

    Update script.js
    Initialize Leaflet map and added fetch request for CTA tracker API

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,21 @@
 // Housekeeping
 const apiKey = "5e45022e107345de8ab3edb8a97f9b94"
+
+
+// Leaflet Map API
+let mymap = L.map('mapid').setView([41.8781, -87.6298], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+
+
+// Fetch the data from the CTA API
+fetch('https://api.transitchicago.com/api/1.0/cta/' + API_ENDPOINT)
+  .then(response => response.json())
+  .then(data => {
+    // Process the data and update the map or display the train information
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
