6a82cf7fc4ab0fd4816525b00132512c6e259a52
Drew Jordan <ajordan12@luc.edu>
2023-05-30 18:56:32 -0500

    general updates

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 // Housekeeping
 const ctaKey = "5e45022e107345de8ab3edb8a97f9b94";
 const googleMap =  "AIzaSyA33nfJr_RnsUSUcdi4RmKvxuqfwdSniuc";
+const mapKey = "AIzaSyA33nfJr_RnsUSUcdi4RmKvxuqfwdSniuc";
 // const mapBox = "pk.eyJ1IjoiZHJld2pvcmRhbjQwNCIsImEiOiJjbGlhbnF6bjQwMG1jM2ZuMjRpcHBoeHRyIn0.s6T-06OAwld32_Y9wTKsog";
 const trainDriection = {
     "red":{
@@ -37,10 +38,11 @@ const trainDriection = {
     },
 }
 
+
 //map using google tranit map api
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
+        zoom: 13,
         center: { lat: 41.8781, lng: -87.6298 }, // Center the map to Chicago
     });
   
@@ -49,37 +51,6 @@ function initMap() {
 }
 
 window.initMap = initMap;
-
-
-
-// // Fetch route data from the CTA API for each route.
-// Object.keys(trainDriection).forEach((route) => {
-//     fetch('https://cors-anywhere.herokuapp.com/http://lapi.transitchicago.com/api/1.0/ttpositions.aspx?key=' + ctaKey + '&rt=' + route + '&outputType=JSON')
-//         .then(response => response.json())
-//         .then(data => {
-//             const trains = data.ctatt.route[0].train;
-//             trains.forEach(train => {
-//                 const lat = train.lat;
-//                 const lon = train.lon;
-//                 const direction = trainDriection[route][train.direction]; // replace 'train.direction' with actual property in your data
-//                 const marker = L.marker([lat, lon]).addTo(mymap);
-//                 marker.bindPopup(`<b>${route} Line Train</b><br>Direction: ${direction}`).openPopup();
-//             });
-//         })
-//         .catch(error => console.error('Error:', error));
-// });
-
-// fetch("https://data.cityofchicago.org/api/views/zbnc-zirh/rows.json?accessType=DOWNLOAD")
-//     .then(response => response.json())
-//     .then(data => {
-//         data.forEach(stop => {
-//             const lat = parseFloat(stop.latitude);
-//             const lon = parseFloat(stop.longitude);
-//             const mapMarker = L.marker([lat, lon]).addTo(mymap);
-//             mapMarker.bindPopup(`<b>${stop.stop_name}</b><br>${stop.routes}`).openPopup();
-//         });
-//     })
-//     .catch (error => console.error('Error:', error));
 
 
 
