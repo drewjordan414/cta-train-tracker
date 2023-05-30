c09264dae8d0ca0f8ecac9b3741605ed065af5d0
Drew Jordan <ajordan12@luc.edu>
2023-05-30 14:16:58 -0500

    added mapbox token

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 // Housekeeping
 const apiKey = "5e45022e107345de8ab3edb8a97f9b94"
+const mapBox = "pk.eyJ1IjoiZHJld2pvcmRhbjQwNCIsImEiOiJjbGlhbnF6bjQwMG1jM2ZuMjRpcHBoeHRyIn0.s6T-06OAwld32_Y9wTKsog"
 
 // Leaflet Map
-var mymap = L.map('mapid').setView([41.8781, -87.6298], 13);
+const mymap = L.map('mapid').setView([41.8781, -87.6298], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
