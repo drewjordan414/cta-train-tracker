0e0be2d71c0c4867c18286060c31094908753fe0
Drew Jordan <ajordan12@luc.edu>
2023-05-30 18:15:12 -0500

    google maps tranist overlay

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 // Housekeeping
-const apiKey = "5e45022e107345de8ab3edb8a97f9b94";
+const ctaKey = "5e45022e107345de8ab3edb8a97f9b94";
+const googleMap =  "AIzaSyA33nfJr_RnsUSUcdi4RmKvxuqfwdSniuc";
 // const mapBox = "pk.eyJ1IjoiZHJld2pvcmRhbjQwNCIsImEiOiJjbGlhbnF6bjQwMG1jM2ZuMjRpcHBoeHRyIn0.s6T-06OAwld32_Y9wTKsog";
 const trainDriection = {
     "red":{
@@ -36,40 +37,49 @@ const trainDriection = {
     },
 }
 
-// Leaflet Map
-const mymap = L.map('mapid').setView([41.8781, -87.6298], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+//map using google tranit map api
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 10,
+        center: { lat: 41.8781, lng: -87.6298 }, // Center the map to Chicago
+    });
+  
+    const transitLayer = new google.maps.TransitLayer();
+    transitLayer.setMap(map);
+}
 
-// Fetch route data from the CTA API for each route.
-Object.keys(trainDriection).forEach((route) => {
-    fetch('https://cors-anywhere.herokuapp.com/http://lapi.transitchicago.com/api/1.0/ttpositions.aspx?key=' + apiKey + '&rt=' + route + '&outputType=JSON')
-        .then(response => response.json())
-        .then(data => {
-            const trains = data.ctatt.route[0].train;
-            trains.forEach(train => {
-                const lat = train.lat;
-                const lon = train.lon;
-                const direction = trainDriection[route][train.direction]; // replace 'train.direction' with actual property in your data
-                const marker = L.marker([lat, lon]).addTo(mymap);
-                marker.bindPopup(`<b>${route} Line Train</b><br>Direction: ${direction}`).openPopup();
-            });
-        })
-        .catch(error => console.error('Error:', error));
-});
+window.initMap = initMap;
 
-fetch("https://cors-anywhere.herokuapp.com/https://data.cityofchicago.org/resource/8pix-ypme.json")
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(stop => {
-            const lat = parseFloat(stop.latitude);
-            const lon = parseFloat(stop.longitude);
-            const mapMarker = L.marker([lat, lon]).addTo(mymap);
-            mapMarker.bindPopup(`<b>${stop.stop_name}</b><br>${stop.routes}`).openPopup();
-        });
-    })
-    .catch (error => console.error('Error:', error));
+
+
+// // Fetch route data from the CTA API for each route.
+// Object.keys(trainDriection).forEach((route) => {
+//     fetch('https://cors-anywhere.herokuapp.com/http://lapi.transitchicago.com/api/1.0/ttpositions.aspx?key=' + ctaKey + '&rt=' + route + '&outputType=JSON')
+//         .then(response => response.json())
+//         .then(data => {
+//             const trains = data.ctatt.route[0].train;
+//             trains.forEach(train => {
+//                 const lat = train.lat;
+//                 const lon = train.lon;
+//                 const direction = trainDriection[route][train.direction]; // replace 'train.direction' with actual property in your data
+//                 const marker = L.marker([lat, lon]).addTo(mymap);
+//                 marker.bindPopup(`<b>${route} Line Train</b><br>Direction: ${direction}`).openPopup();
+//             });
+//         })
+//         .catch(error => console.error('Error:', error));
+// });
+
+// fetch("https://data.cityofchicago.org/api/views/zbnc-zirh/rows.json?accessType=DOWNLOAD")
+//     .then(response => response.json())
+//     .then(data => {
+//         data.forEach(stop => {
+//             const lat = parseFloat(stop.latitude);
+//             const lon = parseFloat(stop.longitude);
+//             const mapMarker = L.marker([lat, lon]).addTo(mymap);
+//             mapMarker.bindPopup(`<b>${stop.stop_name}</b><br>${stop.routes}`).openPopup();
+//         });
+//     })
+//     .catch (error => console.error('Error:', error));
 
 
 
