cb40ed035c038210dfd2a4294f38d8376ee5e3f1
Drew Jordan <ajordan12@luc.edu>
2023-06-17 10:01:04 -0500

    file structure

--- server.js
+++ server.js
@@ -1,54 +1,64 @@
-// Import necessary modules and dependencies
-const express = require('express');
-const path = require('path');
+var express = require('express');
+var app = express();
+var path = require('path');
 
-// Create an instance of the Express application
-const app = express();
-
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define route handlers
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Refactor all the route functions into a single one
+function setupLine(google, map, start, end, buttonId, color) {
+  var directionsDisplay;
+  var directionsService = new google.maps.DirectionsService();
 
-function brownLine(google, map) {
-    // Function implementation goes here
-  }
-  
-  function greenLine(google, map) {
-    // Function implementation goes here
-  }
-  
-  function orangeLine(google, map) {
-    // Function implementation goes here
-  }
-  
-  function pinkLine(google, map) {
-    // Function implementation goes here
-  }
-  
-  function purpleLine(google, map) {
-    // Function implementation goes here
-  }
-  
-  function yellowLine(google, map) {
-    // Function implementation goes here
-  }
-  
-  // Add the remaining line functions here
-  
-  // Initialize the map and fetch train data
-  function initMap() {
-    // Code for initializing the map and other functions goes here
-  }
-  
-  window.initMap = initMap;
-  
+  var rendererOptions = {
+    map: map,
+    suppressMarkers: false,
+    polylineOptions: {
+      strokeColor: 'null'
+    }
+  };
+
+  directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+  directionsDisplay.setMap(map);
+
+  var method = 'TRANSIT';
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: google.maps.DirectionsTravelMode[method]
+  };
+
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+
+  // Add an event listener to the button
+  var button = document.getElementById(buttonId);
+  button.addEventListener("click", function() {
+    directionsDisplay.setMap(null);
+    directionsDisplay.setOptions({
+      polylineOptions: {
+        strokeColor: color,
+        strokeWeight: '8',
+        strokeOpacity: '.75',
+      }
+    });
+    directionsDisplay.setMap(map);
+  });
+}
+
+// Use the above function for each line
+setupLine(google, map, "Blue Line, O'Hare Station, 10000 W O'Hare Ave, Chicago, IL 60666", "Blue Line, Forest Park Station, 711 Des Plaines Ave, Forest Park, IL 60130", "blueButton", 'blue');
+// ... repeat for other lines with the appropriate parameters
+
+var server = app.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Listening at http://%s:%s', host, port);
+});
